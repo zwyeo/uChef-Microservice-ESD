83aa29f46b3e163e314a8e43683b6a08d0cd169a
Hoseph132 <joseph.ho1996@gmail.com>
2023-03-12 01:19:05 +0800

    Minor changes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, redirect, request
 
 import stripe
-from torch import device
+
 
 app = Flask(__name__)
 
+@app.route('/')
+def test():
+    print('Hello from stripe')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
