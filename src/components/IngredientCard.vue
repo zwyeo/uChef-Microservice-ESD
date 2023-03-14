ad6898343e71bd66d128b977cb1966e4e57b9be0
zwyeo <>
2023-03-14 17:01:44 +0800

    small edits

---
+++ src/components/IngredientCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card-style">
+    <div class="fw-bold">
+      <img
+        src="../assets/img/core-img/salad.png"
+        class="ingredient-img my-2 mx-2"
+      />
+      {{ ing }}
+      <p>[Show FP logo and price]</p>
+    </div>
+
+    <div class="d-flex">
+      <div class="ms-5 me-2">Qty: {{ qty }}</div>
+      <button class="btn btn-primary me-1" @click="increaseQty()">+</button>
+      <button class="btn btn-secondary me-2" @click="decreaseQty()">-</button>
+      <button class="btn btn-danger" @click="remove()">X</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      qty: 1,
+    };
+  },
+  props: ["ing"],
+  methods: {
+    increaseQty() {
+      this.qty++;
+    },
+    decreaseQty() {
+      if (this.qty != 0) this.qty--;
+    },
+    remove() {},
+  },
+};
+</script>
+
+<style scoped>
+.card-style {
+  cursor: pointer;
+  font-size: 16px;
+  /* background-color: #f6f6c9; */
+  color: #40ba37;
+  border-radius: 5px;
+  box-shadow: 7px 6px 28px 1px rgba(0, 0, 0, 0.24);
+  cursor: pointer;
+  outline: none;
+  transition: 0.2s all;
+}
+.ingredient-img {
+  width: 50px;
+  position: relative;
+}
+</style>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>Order page</h1>
-  Need to pass ingredient props over
+  <h1 class="text-center mt-5">Order page</h1>
+
+  <button class="btn btn-primary">Proceed to Payment</button>
+
+  <div v-for="ingredient in ingredient_list" :key="ingredient" class="col-sm-3">
+    <ingredient-card :ing="ingredient"></ingredient-card>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import IngredientCard from "../components/IngredientCard.vue";
 export default {
+  name: "Order",
+  components: {
+    IngredientCard,
+  },
   props: ["id"],
   data() {
     return {
