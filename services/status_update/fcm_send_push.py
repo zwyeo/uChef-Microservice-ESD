5fd854d1b037d9fb02033e6a9cdb21b4f6f13147
UnseenBanana <66821601+UnseenBanana@users.noreply.github.com>
2023-03-22 13:14:55 +0800

    Works bro

--- services/status_update/fcm_send_push.py
+++ services/status_update/fcm_send_push.py
@@ -3,7 +3,7 @@ import fcm_cloud_messaging as fcm
 # token will need to be taken from Android application
 # the application we built on Android Studio will automatically generate and log the token into console everytime we run the app
 # take the token from the android app and throw it inside the list below
-tokens = ["INSERT TOKEN HERE"]
+tokens = ["elR5_cWOQLmFfkfK4ybhPo:APA91bH4ovNfOj-PsHF7HX8OIcz7_V14FQv6ISJ16a90lgMDepi9P5SV4E8H2OSIB5kCOnqsWDjG925L0u5N58V2lafeYO2qimG2BgBW8NRLvchryrvnUKqVdQ5JB83vWoEDI60g3lbT"]
 
 def sendPush(title, msg, registration_token=tokens, dataObject=None):
     fcm.sendPush(title, msg, registration_token)
