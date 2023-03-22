d19529c1ced6e868e021313f2084a44f5e0166c4
jadengohzh <jadengoh1999@gmail.com>
2023-03-22 14:47:16 +0800

    Updated delivery.py to call order.py

--- services/delivery.py
+++ services/delivery.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import stripe
+# import stripe
 import os, sys
 
 import requests
@@ -22,53 +22,32 @@ orderStatus = "http://localhost:5009/orderStatus"
 def place_delivery():
     # data = request.get_json()
     in_data = {
-	"items": [{
-			"itemName": "Apple",
-			"quantity": 5,
-			"price": 1.25
-		},
-		{
-			"itemName": "Kiwi",
-			"quantity": 3,
-			"price": 0.75
-		},
-		{
-			"itemName": "Pear",
-			"quantity": 2,
-			"price": 0.5
-		}
-	]
+   "items":[
+      "Beef",
+      "Vegetable Oil",
+      "Cinnamon Stick",
+      "Cloves",
+      "Star Anise",
+      "Cardamom",
+      "Coconut Cream",
+      "Water",
+      "Tamarind Paste",
+      "Lime",
+      "Sugar",
+      "Challots"
+   ]
 }
-    # print(type(data))
-    # print(data['items'])
-    response = requests.post(order_URL, json=in_data)
-    order_messages = response.json()
-    # print(order_messages)
-    delivery_messages = []
-    items = in_data["items"]
-    print(items)
-    for item in items:
-        # print(item)
-        if any(msg['message'].startswith(f"Order placed successfully with fairprice for {item['itemName']}") for msg in order_messages):
-            response = requests.get(fairprice_URL + '/' + item['itemName'])
-            if response.status_code == 200:
-                stock = response.json()['data']
-                delivery_messages.append({"success": True, "message": f"Delivery for {item['itemName']} is successful from fairprice", "quantity": item['quantity'], "price": stock['price'] * item['quantity']})
-            else:
-                delivery_messages.append({"success": False, "message": f"Delivery for {item['itemName']} failed from fairprice"})
 
-        elif any(msg['message'].startswith(f"Order placed successfully with Cold Storage for {item['itemName']}") for msg in order_messages):
-            response = requests.get(coldStorage_URL + '/' + item['itemName'])
-            if response.status_code == 200:
-                stock = response.json()['data']
-                delivery_messages.append({"success": True, "message": f"Delivery for {item['itemName']} is successful from cold storage", "quantity": item['quantity'], "price": stock['price'] * item['quantity']})
-            else:
-                delivery_messages.append({"success": False, "message": f"Delivery for {item['itemName']} failed from cold storage"})
+    
+    # 1. Invoke the order microservice
+    print('\n-----Invoking order microservice-----')
+    order_call = requests.post(order_URL, json=in_data)
+    order_result = order_call.json()
+    print('order_result:', order_result['success'])
 
-        else:
-            delivery_messages.append({"success": False, "message": f"Delivery for {item['itemName']} failed"})
 
-    return jsonify(delivery_messages)
+    return jsonify(order_result)
+
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
